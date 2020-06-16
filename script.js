5818ec22630a63190d41ac444a607cdd6d9d96db
eugene3231 <47620989+eugene3231@users.noreply.github.com>
2020-06-16 15:57:04 +0800

    add 2 saved profile buttons

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 //select h3
+// var _ = require('lodash');
+// var array = [1,2,3,4,5,6];
+// console.log('answer:', _.without(array,3));
+
+var button1 = document.getElementById("profile1");
+var button2 = document.getElementById("profile2");
+
 var css = document.querySelector("h3");
 var colorleft = document.querySelector(".colorleft");
 var colorright = document.querySelector(".colorright");
@@ -10,6 +17,27 @@ function changeGrad() {
 
     css.textContent = body.style.background + ";";
 }
+
+function changeGradSaved1() {
+    console.log("click is working");
+    colorleft.value = "#0011ff";
+    colorright.value = "#ff0000";
+    changeGrad();
+}
+
+function changeGradSaved2() {
+    console.log("click is working");
+    colorleft.value = "#002AFF";
+    colorright.value = "#11FF00";
+    changeGrad();
+}
+
+
+
+button1.addEventListener("click", changeGradSaved1);
+
+button2.addEventListener("click", changeGradSaved2);
+
 
 // colorleft.addEventListener("input", changeGrad);
 
